036bb17abb6074d458feff5e7597fb12d9780529
Dakota Turk <dturk0610@gmail.com>
2021-11-16 21:36:25 -0500

    Slowly implementing data api

---
+++ app/src/main/java/com/example/touchanalytics/OpenSaveCSV.java
@@ -0,0 +1,23 @@
+package com.example.touchanalytics;
+
+public class OpenSaveCSV {
+
+
+    /*
+    try
+    {
+        //parsing a CSV file into BufferedReader class constructor
+        BufferedReader br = new BufferedReader(new FileReader("CSVDemo.csv"));
+        while ((line = br.readLine()) != null)   //returns a Boolean value
+        {
+            String[] employee = line.split(splitBy);    // use comma as separator
+            System.out.println("Employee [First Name=" + employee[0] + ", Last Name=" + employee[1] + ", Designation=" + employee[2] + ", Contact=" + employee[3] + ", Salary= " + employee[4] + ", City= " + employee[5] +"]");
+        }
+    }
+    catch (IOException e)
+    {
+        e.printStackTrace();
+    } */
+
+
+}
